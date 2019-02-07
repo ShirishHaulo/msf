3a20f274b66488a0e9da1d99f4ac6115652aa08c
Shirish Haulooman <shirishhaulo@hotmail.com>
2019-02-07 12:17:24 +0400

    First commit

---
+++ spfx-react-search-box-webpart-master-reactsearchboxstrings_en-us_99bb292fcfbc34373a16223d10698137.js
@@ -0,0 +1,10 @@
+define([], function () {
+  return {
+    "PropertyPaneSearchOptions": "Search Box",
+    "BasicGroupName": "Settings",
+    "DefaultSearchResultsPageUrlFieldLabel": "Search Results Page Url",
+    "EnableCustomSearchPlaceholderLabel": "Position below the page actions",
+    "SearchLabel": "Search",
+    "CustomSearchPlaceholderModeOnAlert": "Search Box Custom Mode ON - The webpart is positioned below the actions section on the page."
+  }
+});
